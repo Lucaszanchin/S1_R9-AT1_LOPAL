334ebff8a58b56a6e51074ab3f42ae38cc5e35d9
unknown <akilamvicente@gmail.com>
2025-06-16 16:02:36 -0300

    NomeEndereco

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')();
+const promptSync = require('prompt-sync')();
 const fs = require('fs');
 
 let nomes = [];
@@ -7,3 +7,20 @@ let tipos = [];
 let custos = [];
 let continuar = true;
 let i = 0; 
+let nome;
+let endereco;
+
+while (continuar) {
+    nome = prompt('Nome do cliente: ');
+    endereco = prompt('Endereço do cliente: ');
+   
+    // Inserindo nos vetores
+    nomes[i] = nome;
+    enderecos[i] = endereco;
+  
+    console.log('\n--- Resumo da Entrega ---');
+    console.log('Cliente:', nome);
+    console.log('Endereço:', endereco);
+}
+
+    i++; // Avança o índice
