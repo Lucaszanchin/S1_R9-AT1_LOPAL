0e8cedcfa50f4642f37ebca64f89913e2638b5b4
unknown <enthony157121@gmail.com>
2025-06-16 16:04:43 -0300

    Parte 4

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ let tipos = [];
 let custos = [];
 let continuar = true;
 let i = 0; 
+let historico;
+
+console.log('\n=== Resumo Final ===');
+console.log('Total de entregas:', totalEntregas);
+console.log('Custo total: R$ ' + custoTotal.toFixed(2));
+console.log('Média por entrega: R$ ' + media.toFixed(2));
+
+historico = 'Histórico de Entregas:\n';
+for (let j = 0; j < totalEntregas; j++) {
+    historico += `Cliente: ${nomes[j]}, Endereço: ${enderecos[j]}, Tipo: ${tipos[j]}, Custo: R$ ${custos[j].toFixed(2)}\n`;
+}
+historico += `\nTotal de entregas: ${totalEntregas}\nCusto total: R$ ${custoTotal.toFixed(2)}\nMédia: R$ ${media.toFixed(2)}\n`;
+
+fs.writeFileSync('historico_entregas.txt', historico);
+console.log('\nHistórico salvo no arquivo historico_entregas.txt');
+
+
+
+
